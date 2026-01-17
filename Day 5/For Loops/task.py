949958ede84ca7172abf0d71abb60ea627a7fb9a
CyberWol-12 <divyau0802@gmail.com>
2026-01-17 10:54:32 +0530

    random_password_generator

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,9 +1,5 @@
-# fruits = ["Apple", "Peach", "Pear"]
-# for fruit in fruits:
-#     print(fruit)
-#     print(fruit + "Pie")
-
-sum = 0
-for number in range(1,101):
-    sum += number
-print(sum)
+fruits = ["Apple", "Peach"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie")
+print(fruits)
