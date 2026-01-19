465826271adfe7cbb2f717deba758d83f31742b8
CyberWol-12 <divyau0802@gmail.com>
2026-01-19 11:18:51 +0530

    cipher encode and decode1

--- Day 8/Functions with Inputs/task.py
+++ Day 8/Functions with Inputs/task.py
@@ -1,5 +1,10 @@
-def greet():
-    print("Hello World")
-    print("How Have You  been")
-    print("how you doing")
-greet()
+# def greet():
+#     print("Hello World")
+#     print("How Have You  been")
+#     print("how you doing")
+# greet()
+
+def name_of_student(name):
+    print(f"hello {name}")
+    print(f"How do you do {name}")
+name_of_student("Divya")
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,3 @@
-# Functions with input
-
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
-
-
-greet_with_name("Jack Bauer")
+def greet_with(location,name):
+    print(f"Hello {name}, what is it like in {location}")
+greet_with("abroad","Divya")
