1aaa8bd26d8bc8ce62fe7e50e846341202171878
CyberWol-12 <divyau0802@gmail.com>
2026-01-22 18:00:04 +0530

    debug

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,12 +1,16 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
 
 my_function()
 
-# Describe the Problem - Write your answers as comments:
-# 1. What is the for loop doing?
-# 2. When is the function meant to print "You got it"?
-# 3. What are your assumptions about the value of i?
+# # Describe the Problem - Write your answers as comments:
+# #Divya
+# # 1. What is the for loop doing?
+# # FOR LOOP IS USED TO ITERATE UNTIL IT NOT REACH THE RANGE FROM 1 TO 20
+# # 2. When is the function meant to print "You got it"?
+# WHEN TOUR I == 20 THEN PRINT OUT AS 20
+# # 3. What are your assumptions about the value of i?
+# I HERE SHOULD BE RANGE FROM 1 TO 20
