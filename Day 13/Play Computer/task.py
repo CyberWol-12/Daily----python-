2532d136d10f42451ab6a99502fa234c1715d1c2
CyberWol-12 <divyau0802@gmail.com>
2026-01-22 19:27:37 +0530

    new game

--- Day 13/Play Computer/task.py
+++ Day 13/Play Computer/task.py
@@ -2,5 +2,7 @@ year = int(input("What's your year of birth?"))
 
 if year > 1980 and year < 1994:
     print("You are a millennial.")
+elif year == 1994:
+    print("you are not gen z")
 elif year > 1994:
     print("You are a Gen Z.")
---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,47 @@
+from art import logo,vs
+from game_data import data
+import random
+
+def formate_data(account):
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name},a {account_descr},from {account_country}"
+
+
+def check_answer(user_guess, a_follower, b_follower):
+    if a_follower > b_follower:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+print(logo)
+score = 0
+account_b = random.choice(data)
+game_should_continue = True
+
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+    print(f"Compare A :{formate_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {formate_data(account_b)}.")
+
+    guess = input("Who has more follower?Type 'A' OR Type 'B'").lower()
+
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess,a_follower_count,b_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"You right! and your current score is:{score}")
+    else:
+        print(f"you lose and your current score is:{score}")
+        game_should_continue = False
+
+
