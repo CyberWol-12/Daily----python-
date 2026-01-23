5ae7d28171c1fe4c1c294f4aa591634c0d76b905
CyberWol-12 <divyau0802@gmail.com>
2026-01-23 11:32:22 +0530

    Initial coffee machine python project

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -23,10 +23,52 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"sorry there is not enough {item}")
+            return False
+    return True
+def process_coins():
+    print("Please insert the coins")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?:")) * 0.05
+    total += int(input("how many pennies?:")) * 0.01
+    return total
 
+def is_transaction_successful(money_received,drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost,2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money.")
+        return False
+
+
+
+is_on = True
+while is_on:
+    choice = input("What do you like?(espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water:{resources['water']}ml")
+        print(f"Milk : {resources['milk']}ml")
+        print(f"Coffee:{resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        print(drink)
+        if is_resource_sufficient(drink["ingredients"]):
+             payment = process_coins()
+             is_transaction_successful(payment,drink["cost"])
